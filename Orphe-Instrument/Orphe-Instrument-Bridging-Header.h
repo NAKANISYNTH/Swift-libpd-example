ce5bbc3c5234c94a271708553d5689e8e0929ace
Kyosuke Nakanishi <kyoro920@gmail.com>
2017-01-01 04:24:16 +0900

    pd-for-ios導入

---
+++ Orphe-Instrument/Orphe-Instrument-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Orphe-Instrument-Bridging-Header.h
+//  Orphe-Instrument
+//
+//  Created by kyosuke on 2017/01/01.
+//  Copyright © 2017年 kyosuke. All rights reserved.
+//
+
+#ifndef Orphe_Instrument_Bridging_Header_h
+#define Orphe_Instrument_Bridging_Header_h
+
+// libpd
+#import "PdAudioUnit.h"
+#import "PdAudioController.h"
+#import "AudioHelpers.h"
+#import "PdBase.h"
+
+#endif /* Orphe_Instrument_Bridging_Header_h */
