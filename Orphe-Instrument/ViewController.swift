a77f83cc52b7bf33d5234f459bd442a4f31116b9
Kyosuke Nakanishi <kyoro920@gmail.com>
2017-01-01 02:05:59 +0900

    first commit

---
+++ Orphe-Instrument/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Orphe-Instrument
+//
+//  Created by kyosuke on 2017/01/01.
+//  Copyright © 2017年 kyosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
