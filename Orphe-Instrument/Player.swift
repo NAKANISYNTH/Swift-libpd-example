833fe51eca64c0b04301aa49b70d8e1fb587282f
Kyosuke Nakanishi <kyoro920@gmail.com>
2017-01-02 17:10:25 +0900

    swift2.3に変更。Pdファイルを開いてサイン波を鳴らす。

---
+++ Orphe-Instrument/Player.swift
@@ -0,0 +1,40 @@
+//
+//  Player.swift
+//  Orphe-Instrument
+//
+//  Created by kyosuke on 2017/01/02.
+//  Copyright © 2017年 kyosuke. All rights reserved.
+//
+
+class Player : NSObject, PdReceiverDelegate{
+    
+    let audioController = PdAudioController() // PureData
+    static let sharedInstance = Player()
+    var pdPointer:UnsafeMutablePointer<Void>?
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    func openPdFile(){
+        audioController.configurePlaybackWithSampleRate(44100, numberChannels: 2, inputEnabled: false, mixingEnabled: true)
+        audioController.active = true
+        
+        if pdPointer == nil {
+            
+            pdPointer = PdBase.openFile("pd-patches/main.pd", path: NSBundle.mainBundle().resourcePath)
+            print("open pd file")
+        }
+    }
+    
+    func closePdFile(){
+        if pdPointer != nil{
+            PdBase.closeFile(pdPointer!)
+            pdPointer = nil
+            print("close pd file")
+        }
+        audioController.active = false
+    }
+    
+}
